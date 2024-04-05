30148b7f88f19134f47dc302e69490be047e962e
JonnyOThan <jonnyothan@gmail.com>
2024-04-05 16:23:03 -0400

    first pass at setting up "update version" workflow and action

---
+++ update-version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+shopt -s globstar
+
+VERSION_STRING="$1"; shift
+TEMPLATE_EXTENSION="$1"; shift
+
+declare -A TOKENS
+
+VERSION_FULL="$(echo $VERSION_STRING | tr -cd [\.0-9])"
+TOKENS["VERSION_STRING"]=$VERSION_STRING
+TOKENS["VERSION_FULL"]=$VERSION_FULL
+TOKENS["VERSION_MAJOR"]="$(echo $VERSION_FULL | cut -f 1 -d '.')"
+TOKENS["VERSION_MINOR"]="$(echo $VERSION_FULL | cut -f 2 -d '.')"
+TOKENS["VERSION_PATCH"]="$(echo $VERSION_FULL | cut -f 3 -d '.')"
+TOKENS["VERSION_BUILD"]="$(echo $VERSION_FULL | cut -f 4 -d '.')"
+
+SED_COMMAND="-e "
+
+for KEY in "${!TOKENS[@]}"; do
+    echo "$KEY: ${TOKENS[$KEY]}"
+	SED_COMMAND="${SED_COMMAND}s/@${KEY}@/${TOKENS[$KEY]}/g;"
+done
+
+for FILENAME in $@; do
+	NEW_FILENAME="${FILENAME%$TEMPLATE_EXTENSION}"
+	echo "transforming $FILENAME -> $NEW_FILENAME"
+	sed "${SED_COMMAND}" "${FILENAME}" > "${NEW_FILENAME}"
+done
