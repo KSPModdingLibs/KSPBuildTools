3353cf0cd2cb736508626a8b5eb02d487c5b8024
Andrew Cassidy <drewcassidy@me.com>
2024-09-14 20:45:43 -0700

    here goes nothing

--- Editor/AssetBundleBuilder.cs
+++ Editor/AssetBundleBuilder.cs
@@ -1,7 +1,21 @@
+using System;
 using UnityEditor;
 
-namespace KSPBuildTools.Editor {
-    public class AssetBundleBuilder {
+public class AssetBundleBuilder
+{
+    public static void BuildBundles()
+    {
+        string[] args = Environment.GetCommandLineArgs();
+        string path = "";
+        string target = "";
+        for (int i = 0; i < args.Length; i++)
+        {
+            if(args[i] == "-assetbundlePath")
+            {
+                path = args[i + 1];
+            }
+        }
 
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
     }
 }
