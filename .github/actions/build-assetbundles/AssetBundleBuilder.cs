00276f7fe478c0f0532cb5695c6e54680ccb50fb
Andrew Cassidy <drewcassidy@me.com>
2024-09-17 22:03:11 -0700

    build anonymous asset lists

--- .github/actions/build-assetbundles/AssetBundleBuilder.cs
+++ .github/actions/build-assetbundles/AssetBundleBuilder.cs
@@ -3,23 +3,42 @@ using UnityEditor;
 using UnityEngine;
 
 namespace KSPBuildTools {
-    public class AssetBundleBuilder
-    {
-        public static void BuildBundles()
-        {
+    public class AssetBundleBuilder {
+        public static void BuildBundles() {
             string[] args = Environment.GetCommandLineArgs();
+            bool isProject = true;
+            string assetbundleName = null;
             string path = "";
-            for (int i = 0; i < args.Length; i++)
-            {
-                if(args[i] == "-assetbundlePath")
-                {
+            for (int i = 0; i < args.Length; i++) {
+                if (args[i] == "-assetbundlePath") {
                     path = args[i + 1];
+                    i++;
+                }
+
+                if (args[i] == "-assetbundleName") {
+                    isProject = false;
+                    assetbundleName = args[i + 1];
+                    i++;
                 }
             }
 
             Debug.Log($"Building assetbundle for {path}");
 
-            BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+
+            if (isProject) {
+                BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression,
+                    BuildTarget.StandaloneWindows64);
+            }
+            else {
+                var bundleDefinitions = new AssetBundleBuild[] {
+                    new AssetBundleBuild {
+                        assetBundleName = assetbundleName,
+                        assetNames = new string[] { "Assets/Bundle" }
+                    }
+                };
+                BuildPipeline.BuildAssetBundles(path, bundleDefinitions, BuildAssetBundleOptions.ChunkBasedCompression,
+                    BuildTarget.StandaloneWindows64);
+            }
 
             Debug.Log("Done!");
         }
