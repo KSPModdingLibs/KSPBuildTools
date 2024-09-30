074c7d3592ac2e700a33d5897a74aadf55854037
Andrew Cassidy <drewcassidy@me.com>
2024-09-29 22:40:05 -0700

    Setup docs dev environment

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+from pathlib import Path
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -13,15 +14,18 @@ author = 'KSPModdingLibs Contributors'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_gha', 'myst_parser', 'sphinx_copybutton']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Options for sphinx-gha --------------------------------------------------
+
+sphinx_gha_repo_root = str(Path(__file__).parent.parent.absolute())  # docs/..
+sphinx_gha_repo_slug = 'KSPModdingLibs/KSPBuildTools'
