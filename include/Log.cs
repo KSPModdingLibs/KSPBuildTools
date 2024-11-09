6cbb052b4c39c89e7a4981699daac32ec243de59
Andrew Cassidy <drewcassidy@me.com>
2024-11-08 21:42:44 -0800

    logging extension methods

--- include/Log.cs
+++ include/Log.cs
@@ -5,33 +5,93 @@ using UnityEngine;
 
 namespace KSPBuildTools
 {
-    internal static class Log
-    {
-        internal static string Prefix = "[" + Assembly.GetExecutingAssembly().GetName().Name + "] ";
+    interface ILogContextProvider {
+        string context();
+    }
+	internal static class Log
+	{
+		internal static string ModPrefix = "[" + Assembly.GetExecutingAssembly().GetName().Name + "]";
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static string ObjPrefix(this UnityEngine.Object obj)
+		{
+			return "[" + obj.name + "]";
+		}
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static string ObjPrefix(this ILogContextProvider obj)
+        {
+            return "[" + obj.context() + "]";
+        }
 
 		[System.Diagnostics.Conditional("DEBUG")]
-		internal static void Debug(string message)
+		internal static void Debug(string message, string prefix = "") {
+            UnityEngine.Debug.Log("[DEBUG]" + ModPrefix + prefix + " " + message);
+        }
+
+		[System.Diagnostics.Conditional("DEBUG")]
+		internal static void LogDebug(this UnityEngine.Object obj, string message, string prefix = "") {
+            UnityEngine.Debug.Log( "[DEBUG]" + ModPrefix + obj.ObjPrefix() + prefix +  " " + message, obj);
+        }
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        internal static void LogDebug(this ILogContextProvider obj, string message, string prefix = "") {
+            UnityEngine.Debug.Log("[DEBUG]" + ModPrefix + obj.ObjPrefix() + prefix +  " " + message);
+        }
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static void Message(string message, string prefix = "")
 		{
-			UnityEngine.Debug.Log(Prefix + message);
+			UnityEngine.Debug.Log(ModPrefix + prefix + " " + message);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		internal static void Message(string message)
+        internal static void LogMessage(this UnityEngine.Object obj, string message, string prefix = "")
 		{
-			UnityEngine.Debug.Log(Prefix + message);
+            UnityEngine.Debug.Log(ModPrefix + obj.ObjPrefix() + prefix + " " + message, obj);
+		}
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void LogMessage(this ILogContextProvider obj, string message, string prefix = "")
+        {
+            UnityEngine.Debug.Log(ModPrefix + obj.ObjPrefix() + prefix + " " + message);
+        }
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static void Warning(string message, string prefix = "")
+		{
+			UnityEngine.Debug.LogWarning(ModPrefix + prefix + " " + message);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		internal static void Warning(string message)
+		internal static void LogWarning(this UnityEngine.Object obj, string message, string prefix = "")
 		{
-			UnityEngine.Debug.LogWarning(Prefix + message);
+            UnityEngine.Debug.LogWarning(ModPrefix + obj.ObjPrefix() + prefix + " " + message, obj);
+		}
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void LogWarning(this ILogContextProvider obj, string message, string prefix = "")
+        {
+            UnityEngine.Debug.LogWarning(ModPrefix + obj.ObjPrefix() + prefix + " " + message);
+        }
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal static void Error(string message, string prefix = "")
+		{
+			UnityEngine.Debug.LogError(ModPrefix + prefix + " " + message);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		internal static void Error(string message)
+		internal static void LogError(UnityEngine.Object obj, string message, string prefix = "")
 		{
-			UnityEngine.Debug.LogError(Prefix + message);
+			UnityEngine.Debug.LogError(ModPrefix + obj.ObjPrefix() + prefix + " " + message, obj);
 		}
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void LogError(this ILogContextProvider obj, string message, string prefix = "")
+        {
+            UnityEngine.Debug.LogError(ModPrefix + obj.ObjPrefix() + prefix + " " + message);
+        }
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		internal static void Exception(Exception ex)
