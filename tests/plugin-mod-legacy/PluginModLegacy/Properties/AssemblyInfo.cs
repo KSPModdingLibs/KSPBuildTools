fb4e220303a3037c36c38f07c255583dab4da837
Andrew Cassidy <drewcassidy@me.com>
2025-10-28 14:43:59 -0700

    Revert "Remove support for legacy csproj files"
    This reverts commit 5d58022ab42bd38f8ba275d668cf6dbeac4245d9.

---
+++ tests/plugin-mod-legacy/PluginModLegacy/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle ("PluginModLegacy")]
+[assembly: AssemblyDescription ("Test mod using a legacy csproj")]
+
+// This supposedly helps avoid plugin depencency problems.
+[assembly: KSPAssembly("plugin-mod-legacy", 1, 2, 3)]
